93e0a5e41411efec13fe6a7f5d8e03f602be5fa7
iffarurrahat <iffarurrahat@gmail.com>
2024-04-23 02:01:24 +0600

    new code added on my file

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -7,12 +7,41 @@
 //   playgroundSection.classList.remove("hidden");
 // }
 
-function handleKeyboardButtonPress() {
-  console.log("press it");
+function handleKeyboardKeyUpEvent(event) {
+  const playerPressed = event.key;
+
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+  // console.log(playerPressed, expectedAlphabet);
+
+  // checked match or not
+  if (playerPressed === expectedAlphabet) {
+    //updated score
+    // 1. get current score
+    const currentScoreElement = document.getElementById("current-score");
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScore = parseInt(currentScoreText);
+    console.log(currentScore);
+
+    // 2. increase the score
+    const newScore = currentScore + 1;
+
+    // 3. show update score
+    currentScoreElement.innerText = newScore;
+
+    // start a new round
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+  } else {
+    // step-1: get the current life number
+    // step-2: reduce the life current
+    // step-3: display the updated life count
+  }
 }
 
 // capture keyboard key press
-document.addEventListener("keyup", handleKeyboardButtonPress);
+document.addEventListener("keyup", handleKeyboardKeyUpEvent);
 
 function continueGame() {
   // step-1 : generate random alphabet
--- script/utility.js
+++ script/utility.js
@@ -10,12 +10,12 @@ function showElementById(elementId) {
 
 function setBackgroundColorById(elementId) {
   const element = document.getElementById(elementId);
-  element.classList.add("bg-orange-400");
+  element.classList.add("bg-orange-500");
 }
 
 function removeBackgroundColorById(elementId) {
   const element = document.getElementById(elementId);
-  element.classList.remove("bg-orange-400");
+  element.classList.remove("bg-orange-500");
 }
 
 function getARandomAlphabet() {
